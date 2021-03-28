7387aefeecf68bbbb239e1749eeff996c33bd1f3
Stephen Solka <solka@hey.com>
2021-03-28 10:23:54 -0400

    make href links absolute

--- tests/lib.rs
+++ tests/lib.rs
@@ -12,3 +12,21 @@ fn test_extract_title() {
     let product = readability::extractor::extract(&mut file, &url).unwrap();
     assert_eq!(product.title, "This is title");
 }
+
+#[test]
+fn test_fix_rel_links() {
+    assert!(true);
+    let mut file = File::open("./data/rel.html").unwrap();
+    let url = Url::parse("https://example.com").unwrap();
+    let product = readability::extractor::extract(&mut file, &url).unwrap();
+    assert_eq!(product.content, "<!DOCTYPE html><html><head><title>This is title</title></head><body><p><a href=\"https://example.com/poop\"> poop </a></p></body></html>");
+}
+
+#[test]
+fn test_fix_img_links() {
+    assert!(true);
+    let mut file = File::open("./data/img.html").unwrap();
+    let url = Url::parse("https://example.com").unwrap();
+    let product = readability::extractor::extract(&mut file, &url).unwrap();
+    assert_eq!(product.content, "<!DOCTYPE html><html><head><title>This is title</title></head><body><p><img src=\"https://example.com/poop.png\"></p></body></html>");
+}
